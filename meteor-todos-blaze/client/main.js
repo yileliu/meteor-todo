e1a201efc3ca23b0b671a6f7b3aad8d6f40b9321
Yile Liu <yile.liu@autodesk.com>
2018-01-25 17:11:42 +0800

    meteor todo sample for blazing mode

---
+++ meteor-todos-blaze/client/main.js
@@ -0,0 +1,7 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import '../imports/api/tasks.js';
+import '../imports/startup/accounts-config.js';
+import '../imports/ui/body.js';
+//import './main.html';
